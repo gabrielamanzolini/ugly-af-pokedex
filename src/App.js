10961e3d706f1a3cfa23feb02fdbae1045596de4
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-09-01 17:41:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Router, Switch, Route } from './routing';
+import Home from './Home';
+import Pokemon from './Pokemon';
+
+class App extends Component {
+  state = {
+    selectedPokemon: null
+  };
+
+  selectPokemon = selectedPokemon => {
+    this.setState({
+      selectedPokemon
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Home {...props} selectPokemon={this.selectPokemon} />
+              )}
+            />
+            <Route
+              path="/pokemon"
+              render={props => (
+                <Pokemon
+                  {...props}
+                  selectedPokemon={this.state.selectedPokemon}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 50,
+    backgroundColor: 'red',
+    color: 'yellow',
+  }
+});
+
+export default App;
